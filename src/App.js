7b109e73a7fb32bbf5990223a96b5eb50d50c627
Nisarg Vadgama <nisargvv@gmail.com>
2021-05-03 18:07:54 -0400

    Get movie data from omdb API through axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const fetchData = async () => {
+    let req = await axios.get(`http://www.omdbapi.com/?s=batman&apikey=${process.env.REACT_APP_API_KEY}`);
+    console.log(req.data.Search);
+  }
+
+  fetchData();
+
   return (
     <div className="App">
-      OMDB
+      <h1>The Shoppies</h1>
+
     </div>
   );
 }
