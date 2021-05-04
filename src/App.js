5e55b59fcd253ee63889cfa6fbd9f1bf48bf1a22
Nisarg Vadgama <nisargvv@gmail.com>
2021-05-04 19:17:30 -0400

    Show movies using react hooks

--- src/App.js
+++ src/App.js
@@ -1,19 +1,57 @@
 import './App.css';
 import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function App() {
 
-  const fetchData = async () => {
-    let req = await axios.get(`http://www.omdbapi.com/?s=batman&apikey=${process.env.REACT_APP_API_KEY}`);
-    console.log(req.data.Search);
-  }
+  const [movie, setMovie] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
 
-  fetchData();
+  useEffect(() => {
+    axios.get(`http://www.omdbapi.com/?s=${movie}&apikey=${process.env.REACT_APP_API_KEY}`)
+      .then(function(response) {
+        // handle success
+        // const results = response.data.Search.filter(name =>
+        //   name.toLowerCase().includes(movie.toLowerCase())
+        // )
+        console.log(response.data.Search);
+        setSearchResults(response.data.Search);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+  }, [movie]);
+
+  // const fetchData = async () => {
+  //   let req = await axios.get(`http://www.omdbapi.com/?s=batman&apikey=${process.env.REACT_APP_API_KEY}`);
+  //   console.log(req.data.Search);
+  // }
+
+  // fetchData();
 
   return (
     <div className="App">
       <h1>The Shoppies</h1>
-
+      <input
+        type="text"
+        placeholder="Search..."
+        value={movie}
+        onChange={(e) => {
+          setMovie(e.target.value);
+        }}
+      />
+      {/* <div>{searchResults}</div> */}
+      <ul>
+         {
+          searchResults ?
+          searchResults.map(item => (
+            <li>{item.Title}</li>
+          ))
+          :
+          <div>No Data</div>
+        }
+      </ul>
     </div>
   );
 }
