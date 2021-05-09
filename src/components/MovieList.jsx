b31c1da7b9d9de9164aadabaa269855a23af2da0
Nisarg Vadgama <nisargvv@gmail.com>
2021-05-09 16:58:30 -0400

    Add Card and Image components of Semantic UI

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,31 +1,24 @@
 import React from "react";
-import { Grid, Segment, Card, Image, Button } from 'semantic-ui-react';
+import { Card, Image, Button } from 'semantic-ui-react';
+import '../App.css';
 
 export default function MovieList({ item, index, disableButton, addToNominations, removeNominations }) {
   return (
-    <div>
-      <Grid container columns='equal'>
-        <Grid.Column>
-          <Segment>
-            <Card>
-              <Image src={item.Poster} wrapped ui={false} />
-              <Card.Content>
-                <Card.Header>{item.Title}</Card.Header>
-                <Card.Meta>Released in {item.Year}</Card.Meta>
-              </Card.Content>
-              <Card.Content extra>
-                {/* logic for which button to show */}
-                {
-                  addToNominations ?
-                    <Button inverted color='blue' disabled={disableButton(item)} onClick={() => addToNominations(item)}>Nominate</Button>
-                    :
-                    <Button inverted color='red' onClick={() => removeNominations(index)}>Remove</Button>
-                }
-              </Card.Content>
-            </Card>
-          </Segment>
-        </Grid.Column>
-      </Grid>
+    <div className="App">
+      <Card centered color='yellow'>
+        <Image src={item.Poster} wrapped ui={false} />
+        <Card.Content>
+          <Card.Header>{item.Title}</Card.Header>
+          <Card.Meta>Released in {item.Year}</Card.Meta>
+        </Card.Content>
+        {/* logic for which button to show */}
+        {
+          addToNominations ?
+            <Button attached='bottom' inverted color='blue' disabled={disableButton(item)} onClick={() => addToNominations(item)}>Nominate</Button>
+            :
+            <Button attached='bottom' inverted color='red' onClick={() => removeNominations(index)}>Remove</Button>
+        }
+      </Card>
     </div>
   );
 }
