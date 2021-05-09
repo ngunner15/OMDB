ca6a7f72060c0ad3ec5e487b5520fc04f1d9dd2c
Nisarg Vadgama <nisargvv@gmail.com>
2021-05-09 14:43:15 -0400

    Add Grid, Segment, Card and Image components of Semantic UI

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,17 +1,31 @@
 import React from "react";
-import { Button } from 'semantic-ui-react';
+import { Grid, Segment, Card, Image, Button } from 'semantic-ui-react';
 
 export default function MovieList({ item, index, disableButton, addToNominations, removeNominations }) {
   return (
     <div>
-      <p>{item.Title} ({item.Year})</p>
-      {/* logic for which button to show */}
-      {
-        addToNominations ?
-          <Button inverted color='blue' disabled={disableButton(item)} onClick={() => addToNominations(item)}>Nominate</Button>
-          :
-          <Button inverted color='red' onClick={() => removeNominations(index)}>Remove</Button>
-      }
+      <Grid container columns='equal'>
+        <Grid.Column>
+          <Segment>
+            <Card>
+              <Image src={item.Poster} wrapped ui={false} />
+              <Card.Content>
+                <Card.Header>{item.Title}</Card.Header>
+                <Card.Meta>Released in {item.Year}</Card.Meta>
+              </Card.Content>
+              <Card.Content extra>
+                {/* logic for which button to show */}
+                {
+                  addToNominations ?
+                    <Button inverted color='blue' disabled={disableButton(item)} onClick={() => addToNominations(item)}>Nominate</Button>
+                    :
+                    <Button inverted color='red' onClick={() => removeNominations(index)}>Remove</Button>
+                }
+              </Card.Content>
+            </Card>
+          </Segment>
+        </Grid.Column>
+      </Grid>
     </div>
   );
 }
